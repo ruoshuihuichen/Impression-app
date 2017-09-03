2a21b4785b4fd833e110f9f6193781f7397bae83
ruoshuihuichen <654158856@qq.com>
2017-09-03 15:23:11 +0800

    db

--- src/containers/detail/Detail.js
+++ src/containers/detail/Detail.js
@@ -8,17 +8,17 @@ import Header from "../../components/Header/header";
 export default class Detail extends Component{
     render(){
         return (
-            <div className="content">
               <div className="detail">
                 <Header title="详情"/>
-                <img src="http://img.chufaba.me/poi_comment_75d474ae-b0df-42d6-a6dd-e6196ac9e66a.JPG" alt=""/>
-                <img src="http://img.chufaba.me/poi_comment_75d474ae-b0df-42d6-a6dd-e6196ac9e66a.JPG" alt=""/>
-                <p>
-                  <i>心情:</i>
-                  从7月1日起，教堂内有人学习，只有周日开放。所以我很不幸运，只看到了外面</p>
-
+                  <div className="content">
+                    <img src="http://img.chufaba.me/poi_comment_75d474ae-b0df-42d6-a6dd-e6196ac9e66a.JPG" alt=""/>
+                    <img src="http://img.chufaba.me/poi_comment_75d474ae-b0df-42d6-a6dd-e6196ac9e66a.JPG" alt=""/>
+                    <p>
+                      <i>心情:</i>
+                      从7月1日起，教堂内有人学习，只有周日开放。所以我很不幸运，只看到了外面</p>
+                  </div>
               </div>
-            </div>
+
         )
     }
 }
